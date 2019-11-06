c317ea0c038ae532533b1f6b133d42f8dcfebeb6
Alumno <alumno@laboratorios.urp.edu.pe>
2019-11-05 19:44:11 -0500

    first commit

---
+++ SistemaGeonet/Data/Migrations/20191102204259_[MGeonet].cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SistemaGeonet.Data.Migrations
+{
+    public partial class MGeonet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApellidoMaterno",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApellidoPaterno",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Documento",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdDireccion",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdTipoDocumento",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombres",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Telefono",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApellidoMaterno",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ApellidoPaterno",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdDireccion",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdTipoDocumento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nombres",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Telefono",
+                table: "AspNetUsers");
+        }
+    }
+}
